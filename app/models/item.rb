94dd86cc390d57d8607d34e07099932002164947
usaaki <usaakirururu@gmail.com>
2023-10-31 17:16:22 +0900

    商品情報入力画面の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :charge
+  belongs_to :period
+  belongs_to :status
+  belongs_to :prefecture
+  
+  validates :item_name,     presence: true
+  validates :description,   presence: true
+  validates :price,         presence: true
+  validates :image,         presence: true
+
+  validates :category_id, :status_id, :charge_id, :prefecture_id, :period_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
