51ac75dc4247816e9ff706fcfb52289cdc4013d8
usaaki <usaakirururu@gmail.com>
2023-11-01 12:43:12 +0900

    javascriptの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   
   validates :item_name,     presence: true
   validates :description,   presence: true
-  validates :price,         presence: true
+  validates :price,         presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :image,         presence: true
 
   validates :category_id, :status_id, :charge_id, :prefecture_id, :period_id, numericality: { other_than: 1 , message: "can't be blank"}
