b1c23b33efbbc00f595b7c7d23d1a93fc5e0a10d
usaaki <usaakirururu@gmail.com>
2023-11-02 17:31:44 +0900

    バリデーションの修正(Rubocop)

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,8 +11,8 @@ class Item < ApplicationRecord
 
   validates :item_name,     presence: true
   validates :description,   presence: true
-  validates :price,         presence: true, numericality: { only_integer: true },
-                            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price,         presence: true, 
+                            numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :image,         presence: true
 
   validates :category_id, :status_id, :charge_id, :prefecture_id, :period_id,
