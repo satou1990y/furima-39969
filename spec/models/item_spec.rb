242e00acfe5722efe9e9a7837ceeaaf19eebdf98
usaaki <usaakirururu@gmail.com>
2023-11-02 15:03:02 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -72,23 +72,56 @@ RSpec.describe Item, type: :model do
       it '販売価格が¥299以下では保存できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it '販売価格が¥10,000,000以上では保存できない' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
       it '販売価格が全角数値では保存できない' do
         @item.price = '５００'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is not a number')
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it 'ユーザーが紐付いていなければ投稿できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('User must exist')
+        expect(@item.errors.full_messages).to include("User must exist")
       end
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.category_id = " --- "
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の状態に「---」が選択されている場合は出品できない' do
+        @item.status_id = " --- "
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it '配送料の負担に「---」が選択されている場合は出品できない' do
+        @item.charge_id = " --- "
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Charge can't be blank")
+      end
+      it '発送元の地域に「---」が選択されている場合は出品できない' do
+        @item.prefecture_id = " --- "
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
+        @item.period_id = " --- "
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Period can't be blank")
+      end
+      it '価格に半角数字以外が含まれている場合は出品できない（※半角数字以外が一文字でも含まれていれば良い)' do
+        @item.price = '111a'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+
+
     end
   end
 end
