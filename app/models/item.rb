0098550811285747747f161fc7de1abd4d3ff2e6
usaaki <usaakirururu@gmail.com>
2023-11-02 14:21:22 +0900

    バリデーションの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
 
   validates :item_name,     presence: true
   validates :description,   presence: true
-  validates :price,         presence: true, format: { with: /\A[0-9]+\z/ },
+  validates :price,         presence: true, numericality: { only_integer: true },
                             numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :image,         presence: true
 
