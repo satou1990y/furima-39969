ab1a2c8513568d8a7cd461a2c6041137b376de86
usaaki <usaakirururu@gmail.com>
2023-11-05 13:06:53 +0900

    Rubocop

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
 
   validates :item_name,     presence: true
   validates :description,   presence: true
-  validates :price,         presence: true, 
+  validates :price,         presence: true,
                             numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :image,         presence: true
 
